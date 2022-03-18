6e5b95209378e1d9f8cd33de94c4a888db98f461
pzh <1026673427@qq.com>
2022-03-18 16:27:52 +0800

    初始环境搭建

--- HelloGIt/src/main/java/com/xxx/HelloGit.java
+++
@@ -1,9 +0,0 @@
-package com.xxx;
-
-public class HelloGit {
-    public static void main(String[] args) {
-            System.out.println("Hello Git!");
-            System.out.println("Hello Git!");
-            System.out.println("Hello Git!");
-    }
-}
